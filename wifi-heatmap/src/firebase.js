7ff0bc7efe219f66541baa72fd1ada2d3c828c3b
Usmaan Sayed <161072079+usmaan677@users.noreply.github.com>
2025-06-24 00:16:04 -0500

    name changes

--- wifi-heatmap/src/firebase.js
+++ wifi-heatmap/src/firebase.js
@@ -1,16 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import dotenv from 'dotenv';
-dotenv.config();
+
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_apiKey,
-    authDomain: import.meta.env.VITE_FIREBASE_authDomain,
-    projectId: import.meta.env.VITE_FIREBASE_projectId,
-    storageBucket: import.meta.env.VITE_FIREBASE_storageBucket,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_messagingSenderId,
-    appId: import.meta.env.VITE_FIREBASE_appId,
-  };
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
